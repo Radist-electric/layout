b450006d709fae1411510e784bb83f3701ca9a48
Radist <radist-electric@yandex.ru>
2020-06-14 23:31:35 +0500

    I added slider

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        centerMode: true,
+        centerPadding: "0px"
+    });
+});
